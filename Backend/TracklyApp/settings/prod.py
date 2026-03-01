aa708720928fdfdeeb51db1dfd0dc28390592420
shillxzy <bzae03014@gmail.com>
2026-03-01 16:08:30 +0200

    Prod #2

--- Backend/TracklyApp/settings/prod.py
+++ Backend/TracklyApp/settings/prod.py
@@ -1,18 +1,13 @@
-from .base import *
 import os
 from decouple import config
+from .base import *
 
 DEBUG = False
 
-RENDER_HOST = os.getenv("RENDER_EXTERNAL_HOSTNAME")
+RENDER_HOST = "trackly-0xg3.onrender.com"
+ALLOWED_HOSTS = [RENDER_HOST]
 
-ALLOWED_HOSTS = [
-    RENDER_HOST,
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    f"https://{RENDER_HOST}",
-]
+CSRF_TRUSTED_ORIGINS = [f"https://{RENDER_HOST}"]
 
 DATABASES = {
     "default": {
@@ -24,21 +19,22 @@ DATABASES = {
         "PORT": config("DB_PORT", default="5432"),
         "OPTIONS": {
             "sslmode": "require",
-        }
+        },
     }
 }
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
-    "https://your-frontend.vercel.app",
+    "https://trackly-frontend-f4ri.onrender.com",  # фронтенд
 ]
 
 SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-LOGIN_REDIRECT_URL = "https://your-frontend.vercel.app/home"
-LOGOUT_REDIRECT_URL = "https://your-frontend.vercel.app/login"
+LOGIN_REDIRECT_URL = "https://trackly-frontend-f4ri.onrender.com/home"
+LOGOUT_REDIRECT_URL = "https://trackly-frontend-f4ri.onrender.com/login"
+
 
 CACHES = {
     "default": {
